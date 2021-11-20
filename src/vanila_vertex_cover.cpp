8f187e6862025bd812048992d5f7c1d2a0f90d9c
Yifan Xu <yifanxu@Yifans-iMac.local>
2021-11-19 21:30:07 -0500

    draft

--- src/vanila_vertex_cover.cpp
+++ src/vanila_vertex_cover.cpp
@@ -8,14 +8,14 @@
 #include <time.h>
 #include <unordered_set>
 #include <algorithm>         
-#include "serial_vertex_cover.h"
+#include "vanila_vertex_cover.h"
 
 using namespace std;
 
-unordered_set<Edge> available_edges;
-unordered_set<Vertex> covered_vertex;
-map<int, Vertex> id_to_vertex;
-map<Vertex, unordered_set<Edge>> vertex_to_edges;
+unordered_set<Edge*> available_edges;
+unordered_set<Vertex*> covered_vertex;
+map<int, Vertex*> id_to_vertex;
+map<Vertex*, unordered_set<Edge*> > vertex_to_edges;
 
 void program(){
     
@@ -32,20 +32,20 @@ void program(){
             if (already_handled < num_vertex) {
                 int id = stoi(line.substr(0, i).c_str());
                 int weight = stoi(line.substr(i+1).c_str());
-                Vertex v;
-                v.id = id;
-                v.weight = weight;
-                v.score = 0.0;
+                Vertex *v = (Vertex*) malloc(sizeof(int) * 2 + sizeof(float));
+                v->id = id;
+                v->weight = weight;
+                v->score = 0.0;
                 id_to_vertex[id] = v;
             } else{
                 int id1 = stoi(line.substr(0, i).c_str());
                 int id2 = stoi(line.substr(i+1).c_str());
-                Edge edge;
-                edge.v1 = id_to_vertex[id1];
-                edge.v2 = id_to_vertex[id2];
+                Edge *edge = (Edge*) malloc(sizeof(Vertex) * 2);
+                edge->v1 = id_to_vertex[id1];
+                edge->v2 = id_to_vertex[id2];
                 available_edges.insert(edge);
-                vertex_to_edges[edge.v1].insert(edge);
-                vertex_to_edges[edge.v2].insert(edge);
+                vertex_to_edges[edge->v1].insert(edge);
+                vertex_to_edges[edge->v2].insert(edge);
             }
             already_handled++;
         }
@@ -67,30 +67,31 @@ void program(){
 //             		add_to_cover(w)
 // 			remove all ages w connects
 
-void main_logic(unordered_set<Edge> available_edges){
+void main_logic(unordered_set<Edge*> available_edges){
     int random_idx = rand() % available_edges.size();
-    unordered_set<Edge>::iterator it = available_edges.begin();
+    unordered_set<Edge*>::iterator it = available_edges.begin();
     for(int i = 0; i < random_idx; i++){
         it++;
     }
-    Vertex v1 = (*it).v1;
-    Vertex v2 = (*it).v2;
+    Vertex *v1 = (*it)->v1;
+    Vertex *v2 = (*it)->v2;
     covered_vertex.insert(v1);
     remove_related_edges(v1);
     covered_vertex.insert(v2);
     remove_related_edges(v2);
 }
 
-void remove_related_edges(Vertex v){
-    unordered_set<Edge> neighbors = vertex_to_edges[v];
-    for(auto it = neighbors.begin(); it != neighbors.end(); it++){
-        Vertex v1 = (*it).v1;
-        Vertex v2 = (*it).v2;
+void remove_related_edges(Vertex *v){
+    unordered_set<Edge*> neighbors = vertex_to_edges[v];
+     
+    for(unordered_set<Edge*>:: iterator it = neighbors.begin(); it != neighbors.end(); it++){
+        Vertex *v1 = (*it)->v1;
+        Vertex *v2 = (*it)->v2;
 
-        if(v1.id != v.id) {
-            vertex_to_edges[v1].erase(it);
+        if(v1->id != v->id) {
+            vertex_to_edges[v1].erase(*it);
         } else{
-            vertex_to_edges[v2].erase(it);
+            vertex_to_edges[v2].erase(*it);
         }
     }
     vertex_to_edges[v].clear();
--- src/vanila_vertex_cover.h
+++ src/vanila_vertex_cover.h
@@ -1,3 +1,6 @@
+#ifndef __VANILAVERTEXCOVER_H__
+#define __VANILAVERTEXCOVER_H__
+
 #include <unordered_set>
 #include <map>  
 
@@ -10,10 +13,12 @@ struct Vertex {
 };
 
 struct Edge {
-   Vertex v1;
-   Vertex v2;
+   Vertex *v1;
+   Vertex *v2;
 };
 
 void program();
 void main_logic(unordered_set<Edge> available_edges);
 void remove_related_edges(Vertex v);
+
+#endif
